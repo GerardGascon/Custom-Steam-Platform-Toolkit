becb05f33e69671b52d6c1fbdb03630afb287a2e
Gerard Gascón <ggasconmoline@gmail.com>
2025-12-11 00:14:59 +0100

    test: cleanup all test files after each test to avoid contamination

--- Packages/com.geri.platformtoolkit.steam/Tests/SteamTests.cs
+++ Packages/com.geri.platformtoolkit.steam/Tests/SteamTests.cs
@@ -1,4 +1,6 @@
-﻿using Geri.PlatformToolkit.Steam;
+﻿using System;
+using System.IO;
+using Geri.PlatformToolkit.Steam;
 using NUnit.Framework;
 using Steamworks;
 using Unity.PlatformToolkit;
@@ -16,6 +18,12 @@ namespace Tests {
 		public void Cleanup() {
 			SteamUserStats.ResetAllStats(true);
 			SteamUserStats.StoreStats();
+
+			SteamSaveFileHelper fileHelper = new(SteamUser.GetSteamID());
+			DirectoryInfo di = new(fileHelper.GetPath());
+
+			Array.ForEach(di.GetFiles(), f => f.Delete());
+			Array.ForEach(di.GetDirectories(), f => f.Delete(true));
 		}
 	}
 }
