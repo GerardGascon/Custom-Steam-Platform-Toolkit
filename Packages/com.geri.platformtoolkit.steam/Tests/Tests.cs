43a725029f4b80109a84e9f6b9dde0fc6df7928d
Gerard Gascón <ggasconmoline@gmail.com>
2025-12-10 19:46:22 +0100

    test: capabilities are configured properly

--- Packages/com.geri.platformtoolkit.steam/Tests/Tests.cs
+++ Packages/com.geri.platformtoolkit.steam/Tests/Tests.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using Geri.PlatformToolkit.Steam;
 using NUnit.Framework;
 using Unity.PlatformToolkit;
@@ -16,6 +17,21 @@ namespace Tests {
 		public void SteamSDKInitializesProperly() {
 			AsyncTestDelegate act = PlatformToolkit.Initialize;
 			Assert.That(act, Throws.Nothing);
+		}
+
+		[Test]
+		public async Task CapabilitiesAreProperlyInitialized() {
+			await PlatformToolkit.Initialize();
+
+			Assert.That(PlatformToolkit.Capabilities.Accounts, Is.True);
+			Assert.That(PlatformToolkit.Capabilities.PrimaryAccount, Is.True);
+			Assert.That(PlatformToolkit.Capabilities.AccountPicker, Is.False);
+			Assert.That(PlatformToolkit.Capabilities.InputOwnership, Is.False);
+			Assert.That(PlatformToolkit.Capabilities.PrimaryAccountEstablishLimited, Is.False);
+			Assert.That(PlatformToolkit.Capabilities.AccountSaving, Is.True);
+			Assert.That(PlatformToolkit.Capabilities.AccountAchievements, Is.True);
+			Assert.That(PlatformToolkit.Capabilities.AccountManualSignOut, Is.False);
+			Assert.That(PlatformToolkit.Capabilities.LocalSaving, Is.False);
 		}
 	}
 }
