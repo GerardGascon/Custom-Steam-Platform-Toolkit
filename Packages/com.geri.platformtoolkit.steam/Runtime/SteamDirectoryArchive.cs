777d2d25d848c6ec40e729a2b12c30b2c2e78e12
Gerard Gascón <ggasconmoline@gmail.com>
2025-12-11 00:10:39 +0100

    fix: store data on correct directories

--- Packages/com.geri.platformtoolkit.steam/Runtime/SteamDirectoryArchive.cs
+++ Packages/com.geri.platformtoolkit.steam/Runtime/SteamDirectoryArchive.cs
@@ -7,16 +7,16 @@ using Unity.PlatformToolkit;
 namespace Geri.PlatformToolkit.Steam {
 	internal class SteamDirectoryArchive : AbstractArchive {
 		private readonly SteamSaveFileHelper _saveFileHelper;
-		private readonly string _name;
+		private readonly string _root;
 		private readonly bool _writeable;
 		private readonly bool _exists = true;
 
-		public SteamDirectoryArchive(SteamSaveFileHelper saveFileHelper, string name, bool writeable) {
+		public SteamDirectoryArchive(SteamSaveFileHelper saveFileHelper, string root, bool writeable) {
 			_saveFileHelper = saveFileHelper;
-			_name = name;
+			_root = root;
 			_writeable = writeable;
 
-			string fullPath = Path.Combine(_saveFileHelper.GetPath(), name);
+			string fullPath = Path.Combine(_saveFileHelper.GetPath(), root);
 			if (_saveFileHelper.EnumerateDirectoriesInDirectory().Contains(fullPath))
 				return;
 
@@ -28,11 +28,11 @@ namespace Geri.PlatformToolkit.Steam {
 		public override Task<IReadOnlyList<string>> EnumerateFiles() {
 			if (!_exists)
 				return Task.FromResult<IReadOnlyList<string>>(new List<string>());
-			return Task.FromResult(_saveFileHelper.EnumerateFilesInDirectory(_name));
+			return Task.FromResult(_saveFileHelper.EnumerateFilesInDirectory(_root));
 		}
 
 		protected override async Task<byte[]> GetDataFromStorage(string name) {
-			return await _saveFileHelper.LoadDataFromFile(name);
+			return await _saveFileHelper.LoadDataFromFile(Path.Combine(_root, name));
 		}
 
 		protected override Task WriteDataToStorage(string name, byte[] data) {
@@ -45,10 +45,10 @@ namespace Geri.PlatformToolkit.Steam {
 				return;
 
 			if (!_exists)
-				_saveFileHelper.CreateDirectory(_name);
+				_saveFileHelper.CreateDirectory(_root);
 
 			foreach ((string name, byte[] data) in writtenFiles)
-				await _saveFileHelper.WriteDataToFile(name, data);
+				await _saveFileHelper.WriteDataToFile(Path.Combine(_root, name), data);
 
 			foreach (string deletedFile in deletedFiles)
 				_saveFileHelper.RemoveFile(deletedFile);
