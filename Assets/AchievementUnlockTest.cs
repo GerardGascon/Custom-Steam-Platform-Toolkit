c1dbb9dcfe7ec64badb527754c92746e79b93a52
Gerard Gascón <ggasconmoline@gmail.com>
2025-12-11 12:46:38 +0100

    feat: basic script to test achievement unlock in play mode

---
+++ Assets/AchievementUnlockTest.cs
@@ -0,0 +1,15 @@
+﻿using Unity.PlatformToolkit;
+using UnityEngine;
+
+public class AchievementUnlockTest : MonoBehaviour {
+	private async void Start() {
+		await PlatformToolkit.Initialize();
+	}
+
+	private async void OnGUI() {
+		if (GUI.Button(new Rect(10, 10, 350, 100), "Unlock Achievement")) {
+			IAchievementSystem system = await PlatformToolkit.Accounts.Primary.Current.GetAchievementSystem();
+			system.Unlock("ACH_WIN_ONE_GAME");
+		}
+	}
+}
